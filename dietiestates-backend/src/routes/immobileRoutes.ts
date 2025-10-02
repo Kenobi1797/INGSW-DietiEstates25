c59d237c7f2c63b6a9b1b7da3ea104bc77238132
pietrodok <pietronapoletano98@gmail.com>
2025-10-02 17:38:23 +0200

    Update immobileRoutes.ts

--- dietiestates-backend/src/routes/immobileRoutes.ts
+++ dietiestates-backend/src/routes/immobileRoutes.ts
@@ -4,15 +4,15 @@ import { authMiddleware, roleMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
-// Creazione immobile (solo agenti/admin)
+// Creazione immobile (solo agenti/admin)  
 router.post(
   '/',
   authMiddleware,
-  roleMiddleware('Agente', 'AmministratoreAgenzia'),
+  roleMiddleware('Agente', 'AmministratoreAgenzia'),  // anche i supportadmin?
   createImmobile
 );
 
 // Ricerca immobili (pubblica)
-router.get('/search', searchImmobili);
+router.get('/search', searchImmobili);               // serve autenticazione ?
 
 export default router;
